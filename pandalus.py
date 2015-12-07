d802b8dc8d5beb8c749207520c4fa36d396f25e5
Hiromasa Ihara <iharahiromasa@gmail.com>
2015-12-07 15:31:48 +0000

    optimization4circleci

--- pandalus.py
+++ pandalus.py
@@ -7,22 +7,33 @@
 # License:  MIT License
 # Created:  2014-05-15
 #
+import sys
+import os
+import time
+import json
+import datetime
+import random
 import threading
+import subprocess
+
 from flask import Flask, render_template, request, Response
 from flask.ext.socketio import SocketIO, emit
 
-import sys
-import os
 from scapy.all import *
-import json
-import time
-import datetime
-import subprocess
 import geoip2.database
 
-import random
+randIP = ["8.8.8.8", "2.60.8.8", "1.0.1.1", "203.178.135.4",
+          "93.158.236.1", "1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4", "5.5.5.5"]
 
-randIP = ["8.8.8.8", "2.60.8.8", "1.0.1.1", "203.178.135.4", "93.158.236.1", "1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4", "5.5.5.5"]
+PROTOCOL_NUMBER_IPV4 = 0x0800
+PROTOCOL_NUMBER_IPV6 = 0x86dd
+PROTOCOL_NUMBER_DOT1Q = 0x8100
+
+PROTOCOL_ICMP = 1
+PROTOCOL_IGMP = 2
+PROTOCOL_ICMP6 = 58
+PROTOCOL_TCP = 6
+PROTOCOL_UDP = 17
 
 geoip = geoip2.database.Reader('./GeoLite2-City.mmdb')
 
@@ -31,82 +42,72 @@ retjson = ""
 def pcap2json(f):
     """pcap -> json, print"""
 
-    _IPV4 = 0x0800
-    _IPV6 = 0x86dd
-    _DOT1Q = 0x8100
-    _ICMP = 1
-    _IGMP = 2
-    _TCP = 6
-    _UDP = 17
-    _ESP = 50
-    _AH = 51
-    _MOBILE = 55
-    _ICMP6 = 58
-    _L2TP = 115
 
     pkts = rdpcap(f)
     newj = {}
     newp = []
     for pkt in pkts:
-        TS = pkt.time
-        
-        ### get l3proto ###$
+        field_time = pkt.time
+
+        # get l3proto ###$
         try:
             l3proto = pkt.type
-            if pkt.type == _DOT1Q:
+            if pkt.type == PROTOCOL_NUMBER_DOT1Q:
                 l3proto = pkt[Dot1Q].type
         except:
             continue
-        
+
         ### get srcip, dstip, proto ###
-        if l3proto == _IPV4:
-            SA, DA, PR  = pkt[IP].src, pkt[IP].dst, pkt[IP].proto
-        elif l3proto == _IPV6:
-            SA, DA, PR  = pkt[IPv6].src, pkt[IPv6].dst, pkt[IPv6].nh
+        if l3proto == PROTOCOL_NUMBER_IPV4:
+            field_srcip, field_dstip, field_protocol = pkt[IP].src, pkt[IP].dst, pkt[IP].proto
+        elif l3proto == PROTOCOL_NUMBER_IPV6:
+            field_srcip, field_dstip, field_protocol = pkt[IPv6].src, pkt[IPv6].dst, pkt[IPv6].nh
         else:
             continue
 
         ### get srcport, dstport ###
-        if PR == _ICMP:
-            SP, DP = pkt[ICMP].type, pkt[ICMP].code
-        elif PR == _TCP:
-            SP, DP = pkt[TCP].sport, pkt[TCP].dport
-        elif PR == _UDP:
-            SP, DP = pkt[UDP].sport, pkt[UDP].dport
+        if field_protocol == PROTOCOL_ICMP:
+            field_srcport, field_dstport = pkt[ICMP].type, pkt[ICMP].code
+        elif field_protocol == PROTOCOL_ICMP6:
+            field_srcport, field_dstport = pkt[ICMP6].type, pkt[ICMP6].code
+        elif field_protocol == PROTOCOL_TCP:
+            field_srcport, field_dstport = pkt[TCP].sport, pkt[TCP].dport
+        elif field_protocol == PROTOCOL_UDP:
+            field_srcport, field_dstport = pkt[UDP].sport, pkt[UDP].dport
         else:
             continue
 
-	try:
-		sres = geoip.city(SA)
-	except Exception as e:
-		sres = geoip.city(randIP[random.randint(0, len(randIP)-1)])
-	try:
-		dres = geoip.city(DA)
-	except Exception as e:
-		dres = geoip.city(randIP[random.randint(0, len(randIP)-1)])
+        try:
+            sres = geoip.city(field_srcip)
+        except Exception as e:
+            sres = geoip.city(randIP[random.randint(0, len(randIP) - 1)])
+        try:
+            dres = geoip.city(field_dip)
+        except Exception as e:
+            dres = geoip.city(randIP[random.randint(0, len(randIP) - 1)])
 
-	srccount = sres.country.name
-	srclat = sres.location.latitude
-	srclog = sres.location.longitude
-	dstcount = dres.country.name
-	dstlat = dres.location.latitude
-	dstlog = dres.location.longitude
+        field_srccount = sres.country.name
+        field_srclat = sres.location.latitude
+        field_srclog = sres.location.longitude
+        field_dstcount = dres.country.name
+        field_dstlat = dres.location.latitude
+        field_dstlog = dres.location.longitude
 
         ### print ###
-	newp.append(json.dumps({"time"  :   TS,
-                          "sip"   :   SA,
-                          "dip"   :   DA,
-                          "proto" :   PR,
-                          "sport" :   SP,
-                          "dport" :   DP,
-			  "srccount" : srccount, 
-			  "srclat" : srclat,
-			  "srclog" : srclog,
-			  "dstcount" : dstcount,
-			  "dstlat" : dstlat,
-                	  "dstlog" : dstlog
-			},
-			separators=(',',':')))
+        newp.append(json.dumps({"time": field_time,
+                                "sip": field_srcip,
+                                "dip": field_dstip,
+                                "proto": field_protocol,
+                                "sport": field_srcport,
+                                "dport": field_dstport,
+                                "srccount": field_srccount,
+                                "srclat": field_srclat,
+                                "srclog": field_srclog,
+                                "dstcount": field_dstcount,
+                                "dstlat": field_dstlat,
+                                "dstlog": field_dstlog
+                                },
+                               separators=(',', ':')))
     newj["data"] = newp
     retjson = newj
 
@@ -114,6 +115,7 @@ def pcap2json(f):
     print "emit"
     socketio.emit('pktdata', {'data': retjson})
     print "emit fin"
+
 
 def tcpdump():
     command = 'tcpdump -i eth0 -G 1 -Uw ./traffic/%S.pcap'.split(' ')
@@ -126,9 +128,9 @@ def tcpdump():
 
 def main():
     ### init ###
-    command = ['rm','-rf','./traffic']
+    command = ['rm', '-rf', './traffic']
     subprocess.Popen(command).wait()
-    command = ['mkdir','traffic']
+    command = ['mkdir', 'traffic']
     subprocess.Popen(command).wait()
 
     ### check new file ###
@@ -139,20 +141,22 @@ def main():
     tcpdump()
     time.sleep(1)
     if not os.path.exists(oldfile):
-        oldfile = './traffic/' + ('0' if sec < 10 else '') + str(sec+1) + '.pcap'
+        oldfile = './traffic/' + \
+            ('0' if sec < 10 else '') + str(sec + 1) + '.pcap'
 
     ### main loop ###
     while(True):
         ### check new file ###
         while(True):
             sec = datetime.datetime.now().second
-            newfile = './traffic/' + ('0' if sec < 10 else '') + str(sec) + '.pcap'
+            newfile = './traffic/' + \
+                ('0' if sec < 10 else '') + str(sec) + '.pcap'
             if (oldfile != newfile and os.path.exists(newfile)):
                 break
 
         ### pcap -> json ###
         pcap2json(oldfile)
-        command = ['rm','-rf',oldfile]
+        command = ['rm', '-rf', oldfile]
         subprocess.Popen(command).wait()
         oldfile = newfile
 
@@ -160,23 +164,22 @@ app = Flask(__name__)
 app.debug = True
 socketio = SocketIO(app)
 
-def camera_thread() :
-  main()
+
+def camera_thread():
+    main()
+
 
 @app.route('/')
-def index() :
-  print "return index"
-  return render_template("pandalous.html")
+def index():
+    print "return index"
+    return render_template("pandalous.html")
 
 
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT'))
 
+    t = threading.Thread(target=camera_thread)
+    t.setDaemon(True)
+    t.start()
 
-if __name__ == "__main__" :
-  port = int(os.environ.get('PORT'))
-
-  t=threading.Thread(target=camera_thread)
-  t.setDaemon(True)
-  t.start()
-
-  socketio.run(app, host="0.0.0.0", port=port)
-
+    socketio.run(app, host="0.0.0.0", port=port)
